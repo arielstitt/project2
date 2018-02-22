1d6e42e7bf1f59886ee74be262590e8712dbe5c0
arielstitt <arielstitt@gmail.com>
2018-02-22 11:22:58 -0500

    required expres.Routers in controllers and exported router

--- controllers/childController.js
+++ controllers/childController.js
@@ -2,10 +2,10 @@ const express = require('express')
 
 const router = express.Router()
 
-// get home page
 
-router.get('/', (req, res)=>{
-    res.render('index', {title: 'Express'})
-})
+
+
+
+
 
 module.exports = router
