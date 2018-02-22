ed8feecde7894badf866b773b87b9d29a4a4e80e
arielstitt <arielstitt@gmail.com>
2018-02-22 17:04:01 -0500

    added shelter schema

--- db/schemas/shelterSchema.js
+++ db/schemas/shelterSchema.js
@@ -1,2 +1,16 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const shelterSchema = mongoose.Schema
+
+const shelterSchema = new Schema({
+    addressNumber: Number,
+    streetName: String, 
+    state: String,
+    city: String,  
+    children: [Array]
+})
+
+
+
+
+modules.export = shelterSchema
